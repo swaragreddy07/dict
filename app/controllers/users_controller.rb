fecc0b54d1ae4120c48d481ef5fda104c1ea3189
swaragreddy07 <swaragreddy07@gmail.com>
2022-07-11 13:53:35 +0530

    completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,51 +1,60 @@
 require 'json'
+
 class UsersController < ApplicationController
-    def signup
-    if session[:id]!=nil
-        redirect_to users_index_path 
+  def signup
+    if session[:id]!= nil
+      redirect_to users_index_path 
     end
-end
-    def index
-    if session[:id]==nil
-        redirect_to root_path
-    end
+  end
 
+  def index
+    if session[:id] == nil
+      redirect_to root_path
     end
+    user = User.find(session[:id])
+    date = Date.today.to_s
+    if user.date!= date
+      user.date = date
+      user.total = 0
+      user.save
+    end
+  end
 
-    def home
+  def home
     
-    end
-    def addkey
+  end
+    
+  def addkey
     user=User.find(session[:id])
     keys=JSON.parse user.keys
-    if user.plan == '1' && keys.length()==5
-        redirect_to users_index_path, notice: "you cannot add anymore keys"
-    elsif user.plan == '2' && keys.length()==10
-        redirect_to users_index_path, notice: "you cannot add anymore keys"
+    if user.plan == '1' && keys.length() == 5
+      redirect_to users_index_path, notice: "you cannot add anymore keys"
+    elsif user.plan == '2' && keys.length() == 10
+      redirect_to users_index_path, notice: "you cannot add anymore keys"
     else
-        payload = { "username": user.username,
-                    "password": user.password,
-                     "key_no":  keys.length()+1}
-        secret="ace_collage"
-        token=JWT.encode payload, secret, 'HS256'
-        keys.append(token)
-        added_on = JSON.parse user.added_on
-        added_on.append(Date.today.to_s)
-        count = JSON.parse user.count
-        count.append(0)
-        user.keys = keys.inspect
-        user.added_on = added_on.inspect
-        user.count = count.inspect
-        user.save
-        redirect_to users_index_path
+      payload = { "username": user.username,
+                  "password": user.password,
+                  "key_no":  keys.length()+1 }
+      secret = "ace_collage"
+      token = JWT.encode payload, secret, 'HS256'
+      keys.append(token)
+      added_on = JSON.parse user.added_on
+      added_on.append(Date.today.to_s)
+      count = JSON.parse user.count
+      count.append(0)
+      user.keys = keys.inspect
+      user.added_on = added_on.inspect
+      user.count = count.inspect
+      user.save
+      redirect_to users_index_path
     end
-
-    end
-    def deletekey
-    user=User.find(session[:id])
-    keys=JSON.parse user.keys
-    added_on=JSON.parse user.added_on
-    count=JSON.parse user.count
+  end
+  
+  def deletekey
+    user = User.find(session[:id])
+    keys = JSON.parse user.keys
+    added_on = JSON.parse user.added_on
+    count = JSON.parse user.count
     keys.delete_at(params[:id].to_i)
     added_on.delete_at(params[:id].to_i)
     count.delete_at(params[:id].to_i)
@@ -54,52 +63,53 @@ end
     user.count=count
     user.save
     redirect_to users_index_path
-    end
-    def pickplan
+  end
+  
+  def pickplan
 
-    end
-
-    def plan
-    user=User.find(session[:id])
-    plan=params[:plan]
-    if plan!="1" && plan!="2" && plan!="3"
-        redirect_to users_pickplan_path,notice: "please pick a valid plan"
+  end
+  
+  def plan
+    user = User.find(session[:id])
+    plan = params[:plan]
+    if plan!= "1" && plan!= "2" && plan!= "3"
+      redirect_to users_pickplan_path, notice: "please pick a valid plan"
     else
-        user.plan=plan
-        user.save
-        redirect_to users_index_path
+      user.plan = plan
+      user.save
+      redirect_to users_index_path
     end
-
-    end
-    def register
-    date=Date.today.to_s
-    @user=User.new(username: params[:username],password: params[:password],plan: "1",date: date,count: '[]',keys: '[]',added_on: '[]',total: 0,email: params[:email])
+  end
+    
+  def register
+    date = Date.today.to_s
+    @user = User.new(username: params[:username], password: params[:password],plan: "1", date: date, count: '[]', keys: '[]', added_on: '[]', total: 0, email: params[:email])
     if @user.save
-        session[:id]=@user.id
-        redirect_to users_pickplan_path
+      session[:id] = @user.id
+      redirect_to users_pickplan_path
     else
-        render :signup
+      render :signup
     end
-end
-    def login
-        if session[:id]!=nil
-            redirect_to users_index_path 
-        end
+  end
+  
+  def login
+    if session[:id]!=nil
+      redirect_to users_index_path 
     end
-    
-    def logout
-    session[:id]=nil
+  end
+  
+  def logout
+    session[:id] = nil
     redirect_to root_path
-    end
-    
-    def enter
-    user=User.find_by(username: params[:username])
-    if user!=nil && user.password==params[:password]
-        session[:id]=user.id
-        redirect_to users_index_path
+  end
+  
+  def enter
+    user = User.find_by(username: params[:username])
+    if user!= nil && user.password == params[:password]
+      session[:id] = user.id
+      redirect_to users_index_path
     else
-        redirect_to users_login_path,notice: 'invalid username or  password'
+      redirect_to users_login_path, notice: 'invalid username or  password'
     end
-
-end
+  end
 end
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,97 +1,98 @@
 class WordsController < ApplicationController
   def key_is_prsent(api_key)
-    user=User.find(session[:id])
-    keys=JSON.parse user.keys
-    if user.plan=='1'
+    user = User.find(session[:id])
+    keys = JSON.parse user.keys
+    if user.plan == '1'
       usage = 500
-    elsif user.plan=='2'
-      usage=2000
+    elsif user.plan == '2'
+      usage = 2000
     else
-      usage=10000
+      usage = 10000
     end
-    if user.total>=usage
+    if user.total>= usage
       return 1
     else
-    a=false
-    count=JSON.parse user.count
-    for i in 0..keys.length()-1
-      if keys[i]==api_key
-        a=true
-        count[i]=count[i]+1
-        user.count=count.inspect
-        user.total=user.total+1
-        user.save
-        break
+      present = false
+      count = JSON.parse user.count
+      for i in 0..keys.length()-1
+        if keys[i] == api_key
+          present = true
+          count[i] = count[i]+1
+          user.count = count.inspect
+          user.total = user.total+1
+          user.save
+          break
+        end
       end
     end
+    return present
   end
-  return a
-end
   
-  def randomWord()
-    api_key=key_is_prsent(params[:api_key])
-    if api_key==true
-    word = Word.all
-    g=[]
-    word.each do |i|
-      g.append(i.word)
-    end
-    g.shuffle!()
-    return render json: g[0]
-    elsif api_key==1
+  def randomWord
+    api_key = key_is_prsent(params[:api_key])
+    if api_key == true
+      word = Word.all
+      words = []
+      word.each do |i|
+        words.append(i.word)
+      end
+      words.shuffle!()
+      return render json: words[0]
+    elsif api_key == 1
       return render json: "You cannot make anymore api calls today"
     else
-    return render json: 'invalid key'
+      return render json: 'invalid key'
     end
   end
- def definitions
-  api_key=key_is_prsent(params[:api_key])
-  curr_word= params[:word]
-  if api_key==true
 
-    definition=Word.find_by(word: curr_word)
-    if definition!=nil 
+  def definitions
+    api_key = key_is_prsent(params[:api_key])
+    curr_word = params[:word]
+    if api_key == true
+      definition = Word.find_by(word: curr_word)
+    if definition!= nil 
       return render json: definition.def
     else
       return render json: 'word does not exist in our database'
     end
-  elsif api_key==1
-    return render json: "You cannot make anymore api calls today"
+    elsif api_key == 1
+      return render json: "You cannot make anymore api calls today"
     else
-  return render json: 'invalid key'
+      return render json: 'invalid key'
     end
-end
-def examples
-  api_key=key_is_prsent(params[:api_key])
-  curr_word= params[:word]
-  if api_key==true
-    example=Word.find_by(word: curr_word)
-    if example==nil
+  end
+
+  def examples
+    api_key = key_is_prsent(params[:api_key])
+    curr_word = params[:word]
+    if api_key == true
+     example = Word.find_by(word: curr_word)
+    if example == nil
       return render json: 'word does not exist in our database'
     else
       return render json: example.exam 
     end
-  elsif api_key==1
-    return render json: "You cannot make anymore api calls today"
-  else
-    return render json: 'invalid key',status: 401
+    elsif api_key == 1
+      return render json: "You cannot make anymore api calls today"
+    else
+      return render json: 'invalid key', status: 401
+    end
   end
-end
-def related
-  api_key=key_is_prsent(params[:api_key])
-  curr_word= params[:word]
-  if api_key==true
-    related_words=Word.find_by(word: curr_word)
-    if related_words==nil
+
+  def related
+    api_key = key_is_prsent(params[:api_key])
+    curr_word= params[:word]
+    if api_key == true
+      related_words = Word.find_by(word: curr_word)
+    if related_words == nil
       return render json: 'the word does not exist in our database'
     else
       return render json: related_words.related
     end
-  elsif api_key==1
-    return render json: "You cannot make anymore api calls today"
-  else
-    return render json: 'invalid key'
+    elsif api_key == 1
+      return render json: "You cannot make anymore api calls today"
+    else
+      return render json: 'invalid key'
+    end
   end
-end
-
 end
