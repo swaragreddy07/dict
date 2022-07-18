aa3500c52231708bc6fa92e31943960e96e476ec
swaragreddy07 <swaragreddy07@gmail.com>
2022-07-18 15:25:56 +0530

    updated

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,36 +1,25 @@
 class WordsController < ApplicationController
   def randomWord
     api_key = Userkey.validate_key(params[:api_key])
-    if api_key == true
-      all_words = Word.all
-      words = []
-      all_words.each do |word|
-        words.append(word.word)
-      end
-      words.shuffle!()
-      return render json: words[0]
-    elsif api_key == 1
-      return render json: "You cannot make anymore api calls today"
-    else
-      return render json: 'invalid key'
-    end
+    current_word = "white"
+    return render json: Word.validate_word(current_word, "random_word", api_key)
   end
 
   def definitions
     api_key = Userkey.validate_key(params[:api_key])
     current_word = params[:word]
-    return render json: Word.validate_word(current_word, 1, api_key)
+    return render json: Word.validate_word(current_word, "word_definitions", api_key)
   end
 
   def examples
     api_key = Userkey.validate_key(params[:api_key])
     current_word = params[:word]
-    return render json: Word.validate_word(current_word, 2, api_key)
+    return render json: Word.validate_word(current_word, "word_examples", api_key)
   end
 
   def relatedwords
     api_key = Userkey.validate_key(params[:api_key])
     current_word = params[:word]
-    return render json: Word.validate_word(current_word, 3, api_key)
+    return render json: Word.validate_word(current_word, "related_words", api_key)
   end
 end
